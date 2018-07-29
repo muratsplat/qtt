6404079e180c0d5ab1c3e38f396045c5e5b6f1c9
Murat Ödünç <murat.asya@gmail.com>
2018-07-29 16:21:36 +0300

    tries to something..

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
+// Some references
+// - https://github.com/eclipse/mosquitto/blob/master/lib/mqtt3_protocol.h
+// - https://github.com/eclipse/paho.mqtt.golang/blob/master/packets/packets.go#L58
+
 import (
+	"fmt"
+	"io"
+	"log"
 	"net"
 
 	"github.com/eclipse/paho.mqtt.golang/packets"
+	"github.com/muratsplat/qtt/auth"
 	"github.com/muratsplat/qtt/session"
 )
 
@@ -25,34 +33,54 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-
+	try := 0
 	for {
 		packet, err := packets.ReadPacket(conn)
 		if err != nil {
+			if err == io.EOF {
+				conn.Close()
+			}
 
+			panic(err)
 		}
 
 		switch v := packet.(type) {
 		case *packets.ConnectPacket:
-			if v.ProtocolName == "MQTT" {
-				if v.ProtocolVersion == 4 {
-					resp := packets.NewControlPacket(packets.Connack)
-					err = resp.Write(conn)
+			try++
+			if v.ProtocolName == "MQTT" || v.ProtocolName == "MQIsdp" {
+				if v.ProtocolVersion == 4 || v.ProtocolVersion == 3 {
+					err := Auth.Check(v.Username, string(v.Password))
 					if err != nil {
-						panic(err)
-					}
-					session.Clients.List[v.ClientIdentifier] = session.NewSession(
-						v.ClientIdentifier,
-						conn,
-					)
-					go session.Clients.List[v.ClientIdentifier].Run()
+						if err == auth.NotAuthErr {
+							unAuth := packets.NewControlPacket(packets.Connack)
+							ackPack := unAuth.(*packets.ConnackPacket)
+							ackPack.ReturnCode = packets.ErrRefusedNotAuthorised
+							fmt.Println(ackPack.String())
+							err := ackPack.Write(conn)
+							if err != nil {
+								log.Println(err)
+							}
+							if try >= 1 {
+								log.Println("Connection is closing")
+								conn.Close()
+								break
+							}
+						}
 
+						log.Println("Connection is closing")
+						conn.Close()
+
+						session.Clients.List[v.ClientIdentifier] = session.NewSession(
+							v.ClientIdentifier,
+							conn,
+						)
+						go session.Clients.List[v.ClientIdentifier].Run()
+					}
 				}
 			}
-
 		}
 	}
 
 }
 
-var AuthSrv IAuth = &Auth{}
+var Auth auth.IAuth = &auth.Auth{}
--- session/session.go
+++ session/session.go
@@ -1,8 +1,12 @@
 package session
 
 import (
+	"fmt"
 	"net"
 	"sync"
+
+	"github.com/eclipse/paho.mqtt.golang/packets"
+	"github.com/muratsplat/qtt/auth"
 )
 
 type Session struct {
@@ -10,22 +14,47 @@ type Session struct {
 	ClientID string
 	Auth     bool
 	sync.RWMutex
-	Done chan bool
+	Done       chan bool
+	User, Pass string
 }
 
 type Connections struct {
 	sync.RWMutex
 	List map[string]*Session
+	Auth auth.IAuth
 }
 
 func (s *Session) Run() {
 	for {
+		packet, err := packets.ReadPacket(s.Conn)
+		if err != nil {
+			panic(err)
+		}
 
+		fmt.Println(packet.String())
+
+		// // Auth Handling
+		// if s.Auth == false {
+		// 	err := Clients.Auth.Check(s.User, s.Pass)
+		// 	if err != nil {
+		// 		unAuth := packets.NewControlPacket(packets.Connack)
+		// 		ackPack := unAuth.(*packets.ConnackPacket)
+		// 		ackPack.ReturnCode = packets.ErrRefusedBadUsernameOrPassword
+		// 		err := ackPack.Write(s.Conn)
+		// 		if err != nil {
+		// 			log.Println(err)
+		// 			log.Println("Connection is closing")
+		// 			s.Conn.Close()
+		// 		}
+		// 	}
+
+		// }
 	}
 }
 
 var Clients = &Connections{
 	List: make(map[string]*Session),
+	Auth: &auth.Auth{},
 }
 
 func NewSession(clientID string, conn net.Conn) *Session {
